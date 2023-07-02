df6670cd7963e4885a93045487545672a13d55e0
prince-sunsara <princesunasara977@gmail.com>
2023-07-02 20:28:23 +0530

    Initial commit

---
+++ src/token_assets/src/components/Faucet.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { token } from "../../../declarations/token";
+
+function Faucet() {
+  const [isDisbaled, setDisable] = useState(false);
+  const [buttonText, setText] = useState("Gimme gimme");
+
+  async function handleClick(event) {
+    setDisable(true);
+    const result = await token.payOut();
+    setText(result)
+    // setDisable(false);
+  }
+
+  return (
+    <div className="blue window">
+      <h2>
+        <span role="img" aria-label="tap emoji">
+          🚰
+        </span>
+        Faucet
+      </h2>
+      <label>Get your free DAngela tokens here! Claim 10,000 PRAN token to your account.</label>
+      <p className="trade-buttons">
+        <button 
+        id="btn-payout" 
+        onClick={handleClick}
+        disabled={isDisbaled}
+        >
+          {buttonText}
+        </button>
+      </p>
+    </div>
+  );
+}
+
+export default Faucet;
